944262745863f0d4e2c196e17591a640d3d99623
Duy <duy@gamifystudios.co>
2024-10-07 10:36:23 +0700

    reorganize external folder

--- openGLCubeDemo/resource/shader/Cube.fs
+++ openGLCubeDemo/resource/shader/Cube.fs
@@ -1,12 +1,12 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 color;
 in vec2 uv;
 
-uniform vec3 uColor;
 uniform sampler2D texture0;
 
 void main()
 {
-    FragColor = uColor.x >= 0 ? vec4(uColor, 1.0f) : texture(texture0, uv);
+    FragColor = color.x >= 0 ? vec4(color, 1.0f) : texture(texture0, uv);
 }
--- openGLCubeDemo/src/main.cpp
+++ openGLCubeDemo/src/main.cpp
@@ -72,11 +72,11 @@ int main()
 void setupVertexData()
 {
 	float vertices[] = {
-		// positions          // texture coords
-		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
-		 0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   -1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+		 0.5f, -0.5f, 0.0f,   -1.0f, 0.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,   -1.0f, 0.0f, 0.0f,   0.0f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f,   -1.0f, 0.0f, 0.0f,   0.0f, 1.0f  // top left 
 	};
 	uint32_t indices[] = {
 		0, 1, 3,  // first Triangle
@@ -96,11 +96,14 @@ void setupVertexData()
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// texture coord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	// texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	// unbind vbo
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
